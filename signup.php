a5a5998d398772829863b82c62b4efab9b1b54c5
Angela <ziemniak357@gmail.com>
2018-07-28 23:46:51 -0500

    signupdate

--- signup.php
+++ signup.php
@@ -12,62 +12,55 @@
 <body>
   <?php
   // define variables and set to empty values
-  $email = $email_err = $passwd = $passwd_err = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $emailErr = $passwdErr = "";
+  $email = $passwd = "";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     if (empty($_POST["email"])) {
-    $email_err = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-  }
+      $emailErr = "Email is required";
+    } else {
+      $email = test_input($_POST["email"]);
+      // check if e-mail address is well-formed
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+      }
+    }
 
-  if (empty($_POST["passwd"])) {
-    $passwd_err = "Password is required";
-  } else {
-    $passwd = test_input($_POST["passwd"]);
-  }
+    if (empty($_POST["passwd"])) {
+      $passwdErr = "";
+    } else {
+      $passwd = test_input($_POST["passwd"]);
+    }
+    if (empty($$passwdErr) && empty($emailErr)){
+      include "database.php";
+      $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
-
+    }
+  }
   function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
   }
-
 ?>
 <div class="container">
  <h1>Hello! Please enter your email address, and a password!</h1>
  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
  <div class="form-group">
    <label for="email">Email address:</label>
-   <input type="email" class="form-control" id="email" placeholder="Please enter your email here!" required><span class="error"><?php echo $email_err;?></span>
+   <input type="email" name="email" class="form-control" id="email" placeholder="Please enter your email here!" required><span class="error"><?php echo $emailErr;?></span>
  </div>
  <div class="form-group">
    <label for="passwd">Password:</label>
-   <input type="password" class="form-control" id="passwd" placeholder="Please enter your password here!" required><span class="error"><?php echo $passwd_err;?></span>
+   <input type="password" name="passwd" class="form-control" id="passwd" placeholder="Please enter your password here!" required><span class="error"><?php echo $passwdErr;?></span>
  </div>
  <button type="submit" class="btn btn-default">Submit</button>
 </form>
 </div>
-<script>
-((function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-</script>
 </body>
 </html>
